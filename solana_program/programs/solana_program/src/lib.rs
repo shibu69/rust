15960c218090c4db43aac0c9b4f339f2dce108e7
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2024-07-22 20:55:26 +0530

    some changes

--- solana_program/programs/solana_program/src/lib.rs
+++ solana_program/programs/solana_program/src/lib.rs
@@ -3,13 +3,40 @@ use anchor_lang::prelude::*;
 declare_id!("ByuDmqDW9hMrU4LKk3XfBekrXRjSV2f8Hjk3ZMRcjD4R");
 
 #[program]
-pub mod solana_program {
+mod solana_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, instruction_data: u64) -> Result<()> {
+        ctx.accounts.counter.count = instruction_data;
         Ok(())
+    }
+
+    pub fn increment(ctx: Context<UpdateAccounts>)-> Result<()>{
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter :{}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("counter incremented. current counter : {}",counter.count);
+        Ok(());
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, seeds = [b"my_seed", user.key.to_bytes().as_ref()], payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateAccounts<'info>{
+    #[account(mut)]
+    pub counter : Account<'info,Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter{
+    count :u64
+}
